c09d20e173732c2e1129fc3f1c185f96b9ec3838
aikin <1@aikin.me>
2018-06-28 23:50:49 +0800

    feat(SHOPPING CART): should can get shopping cart item

--- src/main/java/me/aikin/bicyclestore/order/repository/ShoppingCartItemRepository.java
+++ src/main/java/me/aikin/bicyclestore/order/repository/ShoppingCartItemRepository.java
@@ -5,11 +5,18 @@ import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface ShoppingCartItemRepository {
-   @Select("SELECT * FROM SHOPPING_CART_ITEM WHERE ID=#{ID}")
-   ShoppingCartItem findOne(@Param("ID") Long id);
+    @Results({
+        @Result(property = "id", column = "ID"),
+        @Result(property = "userId", column = "USER_ID"),
+        @Result(property = "bicycleId", column = "BICYCLE_ID"),
+        @Result(property = "quantity", column = "QUANTITY"),
+        @Result(property = "createdAt", column = "CREATED_AT"),
+    })
+    @Select("SELECT * FROM SHOPPING_CART_ITEM WHERE ID=#{ID}")
+    ShoppingCartItem findOne(@Param("ID") long id);
 
     @Insert("INSERT into SHOPPING_CART_ITEM(ID, USER_ID, BICYCLE_ID, QUANTITY, CREATED_AT) VALUES(#{id}, #{userId}, #{bicycleId}, #{quantity}, #{createdAt})")
-    @Options(useGeneratedKeys=true)
+    @Options(useGeneratedKeys = true)
     void save(ShoppingCartItem shoppingCartItem);
 
 }
