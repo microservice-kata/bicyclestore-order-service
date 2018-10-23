aca8c81412cf7dabfc1a4cafe04e868aaf708f79
aikin <1@aikin.me>
2018-10-23 18:58:32 +0800

    feat(CONFIG): add secruity response

--- src/main/java/me/aikin/bicyclestore/order/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/me/aikin/bicyclestore/order/security/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,10 @@
 package me.aikin.bicyclestore.order.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -9,14 +13,28 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
 
 @Slf4j
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
 
         log.error("Responding with unauthorized error. Message : {}", authException.getMessage());
+
+        log.warn("responding with unauthorized error: Message={}", authException.getMessage());
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+
+        HashMap map = new HashMap<String, String>();
+        String errorResponseString = objectMapper.writeValueAsString(map);
+        response.getWriter().print(errorResponseString);
+
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
             "Sorry, You're not authorized to access this resource.");
     }
